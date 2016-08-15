08c8da71ab045210286ffffbe450f77fa1ce9e4c
Carmen Long <carmenbourlonlong@gmail.com>
2016-08-14 23:36:21 -0500

    this works. im quitting before I break it again

--- windows-communications/main.js
+++ windows-communications/main.js
@@ -1,22 +1,25 @@
-const electron = require('electron')
+var electron = require('electron');
 // Module to control application life.
-const app = electron.app
+var app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+var BrowserWindow = electron.BrowserWindow;
+var ipcMain = electron.ipcMain;
+
+console.log('ipcMain', ipcMain);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+var mainWindow;
+var nameWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -24,13 +27,13 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
-  })
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -39,15 +42,32 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+ipcMain.on('open-name-window', function() {
+  if(nameWindow != undefined) {
+    return;
+  }
+  nameWindow = new BrowserWindow({ width: 400, height: 300 });
+  nameWindow.loadURL(`file://${__dirname}/changeName.html`);
+  nameWindow.on('closed', function() {
+    nameWindow = null;
+  });
+});
+
+ipcMain.on('update-the-name', function(event, name) {
+  console.log('seeing update name in mainjs');
+  mainWindow.webContents.send('render-the-name', name);
+  nameWindow.close();
+});
